e46a79ce9f4441666d2ec9f191428c666eb51cbc
mefistofelus <manilo@ukr.net>
2020-07-06 00:34:32 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+"use strict";
+
+const {src, dest} = require("gulp");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const removeComments = require("gulp-strip-css-comments");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const gcmq = require("gulp-group-css-media-queries");
+const cssnano = require("gulp-cssnano");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const panini = require("panini");
+const browsersync = require("browser-sync").create();
+
+let path = {
+  build:{
+    html: "dist/",
+    js: "dist/assets/js/",
+    css: "dist/assets/css/",
+    images: "dist/assets/img/"
+  },
+  src:{
+    html: "src/*.html",
+    js: "src/assets/js/*.js",
+    css: "src/assets/scss/style.scss",
+    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico,webp}"
+  },
+  watch:{
+    html: "src/**/*.html",
+    js: "src/assets/js/**/*.js",
+    css: "src/assets/scss/**/*.scss",
+    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico,webp}"
+  },
+  clean: "./dist"
+}
+
+function browserSync(done) {
+  browsersync.init({
+    server:{
+      baseDir: "./dist/"
+    },
+    port: 3000
+  });
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+}
+
+function html() {
+  panini.refresh();
+  return src(path.src.html, { base: "src/" })
+    .pipe(plumber())
+    .pipe(panini({
+      root: 'src/',
+      layouts: 'src/templates/layouts/',
+      partials: 'src/templates/partials/',
+      helpers: 'src/templates/helpers/',
+      data: 'src/templates/data/'
+    }))
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
+}
+
+function css() {
+  return src(path.src.css, { base: "src/assets/scss/" })
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gcmq())
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      cascade: true
+    }))
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(cssnano({
+      zindex: false,
+      discardComments: {
+        removeAll: true
+      }
+    }))
+    .pipe(removeComments())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".css"
+    }))
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
+}
+
+function js() {
+  return src(path.src.js)
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
+}
+
+function images() {
+  return src(path.src.images, { base: "./src/assets/img/" })
+    .pipe(imagemin())
+    .pipe(dest(path.build.images));
+}
+
+function clean() {
+  return del(path.clean);
+}
+
+function watchFiles() {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.images], images);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images));
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
